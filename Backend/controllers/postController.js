8c95b5d837fda40dab1f111703f1a0278eefbcf4
BekaluEshete <bekelueshete@gmail.com>
2025-07-02 16:56:14 +0300

    Temporary commit for postController.js changes

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -573,42 +573,13 @@ const getPostComments = async (req, res) => {
   }
 }
 
-const getAllPosts = async (req, res) => {
-  try {
-    const posts = await Post.find({ isHidden: false, isPublished: true })
-      .populate("author", "firstName lastName profilePicture")
-      .sort({ createdAt: -1 });
 
-    const formattedPosts = posts.map((post) => {
-      const postObj = post.toObject();
-      if (postObj.isAnonymous) {
-        postObj.author = {
-          firstName: "Anonymous",
-          lastName: "Sister",
-          profilePicture: null,
-        };
-      }
-      return postObj;
-    });
-
-    res.json({
-      success: true,
-      data: formattedPosts,
-    });
-  } catch (error) {
-    console.error("Get all posts error:", error);
-    res.status(500).json({
-      success: false,
-      message: "Failed to fetch all posts",
-      error: error.message,
-    });
-  }
-};
 
 module.exports = {
   getAllPosts,
   createPost,
   getPostsFeed,
+  
   getPost,
   toggleLike,
   addPrayer,
