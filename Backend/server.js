287a8d4ebbb6a7c05d74b17c1d4ff226042494dd
Temesgen Marie <tommr2323@gmail.com>
2025-07-03 14:13:52 +0300

    rate limter removed

--- Backend/server.js
+++ Backend/server.js
@@ -44,12 +44,7 @@ const io = new Server(server, {
 
 app.use(cookieParser());
 app.use(helmet());
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
-app.use(limiter);
-
+ 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
