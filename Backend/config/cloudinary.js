30653e8ec6d3c6891bae219115ba585884de7f0e
Temesgen Marie <155951093+Temsegn@users.noreply.github.com>
2025-07-05 14:47:42 +0300

    Update cloudinary.js

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary = require("cloudinary").v2
 
 cloudinary.config({
   cloud_name: "dsnapr9vc",
-  api_key: "c",
+  api_key: "575263668348966",
   api_secret: "911tdTSWNGdVTvXSLqFYeATqZlA",
 })
 
