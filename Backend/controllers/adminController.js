789bd5bdf591c7028e7975f967431529fa9ad563
Temesgen Marie <tommr2323@gmail.com>
2025-07-07 09:22:39 +0300

    admin controler updated

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -691,7 +691,7 @@ const getDashboardStats = async (req, res) => {
     // Get user statistics
     const totalUsers = await User.countDocuments()
     const activeUsers = await User.countDocuments({ status: "active", isAdminVerified: true })
-    const pendingVerifications = await User.countDocuments({ verificationStatus: "pending", isEmailverified:true })
+    const pendingVerifications = await User.countDocuments({ verificationStatus: "pending", isEmailVerified:true })
     const blockedUsers = await User.countDocuments({ status: "blocked" })
 
     // Get content statistics
