3e46ce152d8b37738a2807b1122b42e0863abcb1
Temesgen Marie <tommr2323@gmail.com>
2025-07-07 22:39:53 +0300

    emailservice updated

--- Backend/utils/emailService.js
+++ Backend/utils/emailService.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD,
+    user: "tommr2323@gmail.com",
+    pass: "ihes tjso xeff afdz",
   },
 });
 
